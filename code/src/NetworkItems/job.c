37548810f5f7a7045605738218846ebc0f6a83c6
DANIEL\Daniel Cauchi <daniel.cauchi.17@um.edu.mt>
2019-05-02 22:50:44 +0200

    Continued doing job functionality

--- code/src/NetworkItems/job.c
+++ code/src/NetworkItems/job.c
@@ -1,20 +1,78 @@
 #include "job.h"
+#include <string.h>
 
 void jobs_init(){
+	sem_init(&batch_jobs_mutex, 0, 1); 
 	sem_init(&jobs_mutex, 0, 1); 
 }
+
+void* addBatchJob(void *newJob){
+	sem_wait(&batch_jobs_mutex);
+
+	numberOfBatchJobs++;
+	batchJobs = (Job*)malloc( numberOfBatchJobs * sizeof(Job) );
+
+	if(numberOfBatchJobs > 1){
+		if( timeBiggerThan( jobs[numberOfBatchJobs - 2].dateTime, (Job*)newJob->dateTime) ){
+			jobs[numberOfBatchJobs - 1] = *(Job*)newJob;
+		}else{
+			jobs[numberOfBatchJobs - 1] = jobs[numberOfBatchJobs - 2];
+		}
+	}else{
+		jobs[numberOfBatchJobs - 1] = *(Job*)newJob;
+	}
+
+	sem_post(&batch_jobs_mutex);
+}
+
+void* removeTopJob(){
+	sem_wait(&batch_jobs_mutex);
+
+	numberOfBatchJobs--;
+	batchJobs = (Job*)malloc( numberOfBatchJobs * sizeof(Job) );
+
+	sem_post(&batch_jobs_mutex);
+}
+
 void* addJob(void *newJob){
 	sem_wait(&jobs_mutex);
 
-	//malloc, sort and set next job
+	Job *j = (Job*)newJob;
+	FILE *f;
+
+	if( !(f=fopen(JOBS_FILENAME, "a")) ) {
+    perror("Error opening file.");
+	}
+
+	char[STRING_BUFFER_SIZE] newLine;
+
+	char[STRING_BUFFER_SIZE] dateTimeString;
+
+	sprintf(dateTimeString, "%d/%d/%d %d:%d:%d", 
+			j->dateTime->tm_mday, j->dateTime->tm_mon, j->dateTime->tm_year,
+			j->dateTime->tm_hour, j->dateTime->tm_min, j->dateTime->tm_sec);
+
+	strcpy(newLine, j->jid);
+	strcat(newLine, " ");
+	strcpy(newLine, j->host);
+	strcat(newLine, " ");
+	strcpy(newLine, j->command);
+	strcat(newLine, " ");
+	strcpy(newLine, j->type);
+	strcat(newLine, " ");
+	strcpy(newLine, j->status);
+	strcat(newLine, " ");
+	strcpy(newLine, dateTimeString);
+	strcat(newLine, "\n");
+
+	fprintf(f, "%s", newLine);
 
 	sem_post(&jobs_mutex);
 }
-void* removeTopJob(){
-	sem_wait(&jobs_mutex);
 
-	sem_post(&jobs_mutex);
-}
+void* getJob(void *jid){}
+
 void jobs_finish(){
+	sem_destroy(&batch_jobs_mutex);
 	sem_destroy(&jobs_mutex);
 }
--- code/src/NetworkItems/job.h
+++ code/src/NetworkItems/job.h
@@ -2,24 +2,40 @@
 #define JOB_H
 
 #include "../MyTime/MyTime.h"
+#include "networkOptions.h"
+
+#define JOBS_FILENAME "Jobs.txt" // file stored in master. Keeps record of all jobs
+#define STRING_BUFFER_SIZE 256
 
 // state of a job
 typedef enum {WAITING, RUNNNING, TERMINATED, FINISHED} JobState;
+typedef enum {Interactive, Batch} Type;
 
-Job *jobs;
+Job *batchJobs;
 Job *nextjob;
-int numberOFJobs;
+int numberOfJobs = 0;
+int numberOfBatchJobs = 0;
 
+sem_t batch_jobs_mutex;
 sem_t jobs_mutex;
 
 typedef struct{
-	int jid; // the job pid
 	int pid; // the actual pid of the process
+	int jid; // the job pid on the network
+	char[STRING_BUFFER_SIZE] host;
+	char[STRING_BUFFER_SIZE] command;
+	Type type;
 	JobState state;
+	struct mt_time *dateTime;
 } Job;
 
+// batch jobs list for each server
 void jobs_init(); // initialize semaphore
-void* addJob(void *newJob); // insert item and sort, and set next job
+void* addBatchJob(void *newJob); // insert item and sort, and set next job
 void* removeTopJob(); // remove the item at the last
 void jobs_finish(); // do at exit
+
+void* addJob(void *newJob); // to the job file in the master
+void* getJob(void *jid); // get a particular job given its id
+
 #endif // JOB_H
